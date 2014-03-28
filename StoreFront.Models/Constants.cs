899304596f0eb099c90b665f3509e0ef3744f8c5
JoshuaRose <joshuarose@outlook.com>
2014-03-27 23:47:42 -0400

    Added constants

---
+++ StoreFront.Models/Constants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreFront.Models
+{
+    public class Constants
+    {
+        public const string GOLD = "Gold";
+        public const string HELIUM = "Helium";
+        public const string CADMIUM = "Cadmium";
+        public const string ALCHEMY = "Alchemy";
+    }
+}
